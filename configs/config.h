c3332859f6ed9c043997292578ad6c594bb4494f
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-09 08:17:36 -0400

    Update tags array with numeric values

--- configs/config.h
+++ configs/config.h
@@ -19,7 +19,12 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "4", "5", "6", "7", "8", "9" };
+// ICONS
+// static const char *tags[] = { "", "", "", "4", "5", "6", "7", "8", "9" };
+// PHRASE
+// static const char *tags[] = { "dev", "www", "sys", "vbox", "mus", "vid", "chat", "doc", "gfx" };
+// NUMBERS
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
