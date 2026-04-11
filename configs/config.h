1443c73364a52fdc5506595d6e615d21a2bc1d97
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-10 23:29:12 -0400

    Update icon tags in config.h

--- configs/config.h
+++ configs/config.h
@@ -20,11 +20,11 @@ static const char *colors[][3]      = {
 
 /* tagging */
 // ICONS
-// static const char *tags[] = { "󱃖", "󰾔", "󰒔", "󰃔", "󰎄", "󰃽", "", "󰧭", "󰈥" };
+static const char *tags[] = { "󰅩", "󰖟", "", "󱌖", "", "", "󱋊", "󰧮", "" };
 // PHRASE
 // static const char *tags[] = { "dev", "www", "sys", "vbox", "mus", "vid", "chat", "doc", "gfx" };
 // NUMBERS
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
