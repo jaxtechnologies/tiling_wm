b79de91db7f1d21ebc8da6e575534cac7f17715b
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-17 13:49:06 -0400

    Toggle comment on tags array in config.h

--- dwm/config.h
+++ dwm/config.h
@@ -22,9 +22,9 @@ static const char *colors[][3]      = {
 // ICONS
 // static const char *tags[] = { "󰅩", "󰖟", "", "󱌖", "", "", "󱋊", "󰧮", "" };
 // PHRASE
-static const char *tags[] = { "1-dev", "2-www", "3-sys", "4-vbox", "5-mus", "6-vid", "7-chat", "8-doc", "9-gfx" };
+// static const char *tags[] = { "1-dev", "2-www", "3-sys", "4-vbox", "5-mus", "6-vid", "7-chat", "8-doc", "9-gfx" };
 // NUMBERS
-// static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
