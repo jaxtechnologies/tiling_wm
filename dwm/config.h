a3008a29c5cbccc7b4405478819a53e3058c3d9c
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-17 16:53:37 -0400

    Add NotoColorEmoji font to font configuration

--- dwm/config.h
+++ dwm/config.h
@@ -5,7 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "mononoki-nerd:size=12" };
+static const char *fonts[]          = { "mononoki-nerd:size=12" 
+									"NotoColorEmoji:size=10:antialias=true:autohint=true" };
 static const char dmenufont[]       = "mononoki-nerd:size=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
