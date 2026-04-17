34d0eb7664e9783856e551577ec0833cbd6e5e0b
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-17 16:36:44 -0400

    Update layout symbols with emoji

--- dwm/config.h
+++ dwm/config.h
@@ -45,9 +45,9 @@ static const int refreshrate = 120;  /* refresh rate (per second) for client mov
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "🐧 ",      tile },    /* first entry is default */
+	{ "👻 ",      NULL },    /* no layout function means floating behavior */
+	{ "🔍 ",      monocle },
 };
 
 /* key definitions */
