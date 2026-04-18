74eed284ef97b53b0826c48b3621026c25dc6d24
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-17 22:15:22 -0400

    Update tags in config.h for dwm

--- dwm/config.h
+++ dwm/config.h
@@ -22,11 +22,11 @@ static const char *colors[][3]      = {
 
 /* tagging */
 // ICONS
-// static const char *tags[] = { "󰅩", "󰖟", "", "󱌖", "", "", "💬", "󰧮", "" };
+// static const char *tags[] = { "󰅩", "🕸", "", "󱌖", "", "", "💬", "󰧮", "" };
 // PHRASE
 // static const char *tags[] = { "1-dev", "2-www", "3-sys", "4-vbox", "5-mus", "6-vid", "7-chat", "8-doc", "9-gfx" };
 // NUMBERS
-static const char *tags[] = { "󾠮", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 static const Rule rules[] = {
 	/* xprop(1):
