8a1202191e9f3916cd22f1112930fef461357d87
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-04-17 21:18:59 -0400

    Modify window rules for st and guvcview

--- dwm/config.h
+++ dwm/config.h
@@ -33,11 +33,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class         instance  title  tags mask  isfloating  isterminal  noswallow  monitor */
-/*	{ "Gimp",        NULL,     NULL,  0,         1,          0,          0,         -1 }, */
-	{ "st",	         NULL,     NULL,  0,         0,          1,         -1,         -1 },
-/*	{ "mpvfloat",    NULL,     "mpv", 0,         1,          0,          0,         -1 }, */
-	{ NULL,  "guvcview", "guvcview",  0,         1,          0,          0,         -1 },
-	{ NULL,     NULL, "Event Tester", 0,         0,          0,          1,         -1 },
+	{ "st",	         NULL,     NULL,  0,         0,          1,          0,         -1 },
 };
 
 /* layout(s) */
