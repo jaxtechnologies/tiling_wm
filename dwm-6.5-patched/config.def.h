e3aa5ecf9a135278550d7c811438b93bc8146d57
Jax|Technologies <104657792+jaxtechnologies@users.noreply.github.com>
2026-05-02 23:11:40 -0400

    Comment out edit and browser command definitions
    Comment out edit and browser commands in config.

--- dwm-6.5-patched/config.def.h
+++ dwm-6.5-patched/config.def.h
@@ -102,15 +102,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
-static const char *editcmd[]  = { "mousepad", NULL };
-static const char *browsercmd[]  = { "firefox", NULL };
+//static const char *editcmd[]  = { "mousepad", NULL };
+//static const char *browsercmd[]  = { "firefox", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = editcmd } },
-	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = browsercmd } },
+//	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = editcmd } },
+//	{ MODKEY|ShiftMask,             XK_b,      spawn,          {.v = browsercmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
