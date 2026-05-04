655c34dbbd48bb06ed60359cfa09e7934d25e143
jaxtechnologies <edwin.reynolds12@gmail.com>
2026-05-04 19:03:32 -0400

    Updated several dwmblock scripts

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,11 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", 	"~/scripts/mod_battery.sh",		60,		0},
 
-	{"💽", 	"~/scripts/jt-disk",		60,		0},
+	{"💽 ", 	"~/scripts/jt-disk",		60,		0},
 
 	{"",	"~/scripts/mod_memory.sh",		30,		0},
 
-	{"🕛", 	"~/scripts/jt-clock",		5,		0},
+	{"🕛 ", 	"~/scripts/jt-clock",		5,		0},
 };
 
 //sets delimiter between status commands. NULL character ('\0') means no delimiter.
